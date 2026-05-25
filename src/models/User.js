6cb0660556dc6cd6fce9ad9e8655a3677e4a250c
emmywennersten <wennerstenemmy@gmail.com>
2026-05-25 11:27:40 +0200

    feat: changed to english message in user-model

--- src/models/User.js
+++ src/models/User.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       unique: true,
-      match: [/^\S+@\S+\.\S+$/, "Ogiltig e-postadress"],
+      match: [/^\S+@\S+\.\S+$/, "Invalid email format"],
     },
     profileImage: {
       type: String, 
